1316880b34b2f1c8d905a808458d4985c2d543c8
SonyVansha <118626845+SonyVansha@users.noreply.github.com>
2023-09-01 15:07:55 +0000

    Perubahan 1

---
+++ php/data.php
@@ -0,0 +1,41 @@
+<?php
+
+
+    // Data dari form yang kita isi
+    $nama = $_POST['nama'];
+            
+    $tanggal = $_POST['tanggal'];
+            
+    $nisn = $_POST['nisn'];
+                        
+    $asal_smp = $_POST['asal']; 
+            
+    $alamat = $_POST['alamat'];
+            
+    $ayah = $_POST['nama_ayah'];
+    
+    $ibu = $_POST['nama_ibu'];
+            
+    $mail = $_POST['email'];
+            
+    $jurusan = $_POST['jurusan'];
+
+?>
+
+
+<!-- Yang akan ditampilkan ketika sudah isi form
+<html> 
+    <head> 
+       <title>Universtitas Sukacita</title> 
+    </head> 
+    <body?> 
+        <h1>Pendaftaran Mahasiswa Baru Universitas Sukacita</h1> 
+        // <?php 
+
+
+            // echo "<p>Terima kasih telah mendaftar" .$nama;
+
+           // echo "<p>Selamat untuk mengikuti jurusan</p>" .$jurusan. "helo";
+        ?> 
+    </body>
+</html> -->
---
+++ php/data_siswa.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <p>halo saya php</p>
+</body>
+</html>
